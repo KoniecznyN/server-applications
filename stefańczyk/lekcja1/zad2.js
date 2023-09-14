a1a0cd457b6843e83107cdb41513badaaf47fcb8
Konieczny Nataniel <nataniel.k08@gmail.com>
2023-09-14 23:06:48 +0200

    zad2

--- stefańczyk/lekcja1/zad2.js
+++ stefańczyk/lekcja1/zad2.js
@@ -2,16 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-// function (q, bgn) {
+function chuj(count, bg) {
+  let dupa = "";
+  for (i = 0; i < count; i++) {
+    dupa +=
+      '<div style="height: 100px; width: 100px; background-color: ' +
+      bg +
+      '; margin: 20px; display: flex; justify-content: center; align-items: center; color: black;">' +
+      (i + 1) +
+      "</div>";
+  }
+  return dupa;
+}
 
-// }
-
-// app.get("/", function (req, res) {
-//   let q = req.query.p1;
-//   let bgn = req.query.p2;
-
-//   // res.send("<div></div>")
-// });
+app.get("/", function (req, res) {
+  console.log(req.query.count);
+  let count = req.query.count;
+  let bg = req.query.bg;
+  res.send(chuj(count, bg));
+});
 
 app.listen(PORT, function () {
   console.log("start");
