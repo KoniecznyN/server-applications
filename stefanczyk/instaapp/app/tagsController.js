0ca11c312beea63ff7cf87e7f52c0a643ba29bcc
KoniecznyN <nataniel.k08@gmail.com>
2024-05-16 11:44:30 +0200

    instaapp cz.3

---
+++ stefanczyk/instaapp/app/tagsController.js
@@ -0,0 +1,10 @@
+const tagsController = {
+    convertTags: (tags) => {
+        let convertedTags = []
+        for (let i = 0; i < tags.length; i++) {
+
+        }
+    }
+}
+
+export { tagsController }
